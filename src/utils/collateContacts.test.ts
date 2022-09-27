1f66f44919570d85dd943d88c16c54ddf98ea1a2
Claudio Rivera <me@claudiorivera.com>
2022-09-26 21:23:20 -0500

    ✨ Finish collateContacts function with sort option

--- src/utils/collateContacts.test.ts
+++ src/utils/collateContacts.test.ts
@@ -1,12 +1,11 @@
-import { Contact } from "@prisma/client";
 import { describe, expect, it } from "vitest";
 
 import { collateContacts } from "./collateContacts";
 
-const contacts: Contact[] = [
+const contacts = [
   {
     id: "a1",
-    firstName: "Alice",
+    firstName: "abby",
     lastName: "Zang",
     email: "",
     phoneNumber: "",
@@ -19,7 +18,7 @@ const contacts: Contact[] = [
   },
   {
     id: "a2",
-    firstName: "Abby",
+    firstName: "Alice",
     lastName: "Anderson",
     email: "",
     phoneNumber: "",
@@ -43,6 +42,19 @@ const contacts: Contact[] = [
     zip: "",
     notes: "",
   },
+  {
+    id: "#2",
+    firstName: "",
+    lastName: "",
+    email: "",
+    phoneNumber: "222-333-4444",
+    address1: "",
+    address2: "",
+    city: "",
+    state: "",
+    zip: "",
+    notes: "",
+  },
 ];
 
 describe("collateContacts", () => {
@@ -54,6 +66,28 @@ describe("collateContacts", () => {
 
   it("should return the correct number of contacts for a given key", () => {
     expect(collateContacts(contacts).A).toHaveLength(2);
-    expect(collateContacts(contacts)["#"]).toHaveLength(1);
+    expect(collateContacts(contacts)["#"]).toHaveLength(2);
+  });
+
+  it("should sort by last name", () => {
+    const sortedContacts = collateContacts(contacts, {
+      sortKey: "lastName",
+    });
+
+    expect(sortedContacts.A && sortedContacts.A[0]?.firstName).toBe("Alice");
+    expect(sortedContacts.A && sortedContacts.A[1]?.firstName).toBe("abby");
+  });
+
+  it("should sort by phone number", () => {
+    const sortedContacts = collateContacts(contacts, {
+      sortKey: "phoneNumber",
+    });
+
+    expect(sortedContacts["#"] && sortedContacts["#"][0]?.phoneNumber).toBe(
+      "222-333-4444"
+    );
+    expect(sortedContacts["#"] && sortedContacts["#"][1]?.phoneNumber).toBe(
+      "555-555-5555"
+    );
   });
 });
--- src/utils/collateContacts.ts
+++ src/utils/collateContacts.ts
@@ -1,9 +1,11 @@
 import { Contact } from "@prisma/client";
 
-export const collateContacts = (contacts: Contact[]) => {
-  return contacts.reduce((obj, contact) => {
-    if (!contact) return obj;
+type Options = {
+  sortKey?: keyof Contact;
+};
 
+export const collateContacts = (contacts: Contact[], options?: Options) => {
+  return contacts.reduce((obj, contact) => {
     const key = contact.firstName?.charAt(0).toUpperCase() || "#";
 
     if (!obj[key]) {
@@ -11,6 +13,15 @@ export const collateContacts = (contacts: Contact[]) => {
     }
 
     obj[key]?.push(contact);
+
+    if (!!options?.sortKey) {
+      obj[key]?.sort(
+        (a, b) =>
+          a[options.sortKey as keyof Contact]?.localeCompare(
+            b[options.sortKey as keyof Contact] || ""
+          ) || 0
+      );
+    }
 
     return obj;
   }, {} as Record<string, Contact[]>);
