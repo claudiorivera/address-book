70a85702e2d2f4d5cd00c54bee83164cf27193bd
Claudio Rivera <me@claudiorivera.com>
2022-10-09 21:09:24 -0500

    💄 Add load bar to update page

--- src/pages/contacts/[contactId]/update.tsx
+++ src/pages/contacts/[contactId]/update.tsx
@@ -55,103 +55,109 @@ const UpdateContactPage = ({ contactId }: Props) => {
 	const isSubmitDisabled = isLoading || !form.formState.isDirty;
 
 	return (
-		<main className="container mx-auto h-screen bg-base-200 p-4">
-			<div className="flex items-center justify-between pb-4 text-secondary">
-				<Link href={`/contacts/${contactId}`}>
-					<a className="flex items-center">
-						<span className="text-sm">Cancel</span>
-					</a>
-				</Link>
-				<button
-					className={classNames("text-sm", {
-						"text-slate-500": isSubmitDisabled,
-					})}
-					form="update-contact"
-					disabled={isSubmitDisabled}
-				>
-					Done
-				</button>
-			</div>
-
-			<form
-				id="update-contact"
-				onSubmit={form.handleSubmit(async (values) => {
-					await updateContact({
-						id: contactId,
-						data: values,
-					});
+		<>
+			<div
+				className={classNames("loading", {
+					"animate-none": !isLoading,
 				})}
-				className="flex flex-col gap-2"
-			>
-				<div className="md:flex">
-					<TextField
-						label="First Name"
-						{...form.register("firstName")}
-						autoComplete="given-name"
-						fieldError={form.formState.errors.firstName}
-					/>
-					<TextField
-						label="Last Name"
-						{...form.register("lastName")}
-						autoComplete="family-name"
-						fieldError={form.formState.errors.lastName}
-					/>
+			/>
+			<main className="container mx-auto bg-base-200 p-4">
+				<div className="flex items-center justify-between pb-4 text-secondary">
+					<Link href={`/contacts/${contactId}`}>
+						<a className="flex items-center">
+							<span className="text-sm">Cancel</span>
+						</a>
+					</Link>
+					<button
+						className={classNames("text-sm", {
+							"text-slate-500": isSubmitDisabled,
+						})}
+						form="update-contact"
+						disabled={isSubmitDisabled}
+					>
+						Done
+					</button>
 				</div>
-				<div className="md:flex">
+				<form
+					id="update-contact"
+					onSubmit={form.handleSubmit(async (values) => {
+						await updateContact({
+							id: contactId,
+							data: values,
+						});
+					})}
+					className="flex flex-col gap-2"
+				>
+					<div className="md:flex">
+						<TextField
+							label="First Name"
+							{...form.register("firstName")}
+							autoComplete="given-name"
+							fieldError={form.formState.errors.firstName}
+						/>
+						<TextField
+							label="Last Name"
+							{...form.register("lastName")}
+							autoComplete="family-name"
+							fieldError={form.formState.errors.lastName}
+						/>
+					</div>
+					<div className="md:flex">
+						<TextField
+							label="Email"
+							{...form.register("email")}
+							autoComplete="email"
+							fieldError={form.formState.errors.email}
+						/>
+						<TextField
+							label="Phone Number"
+							{...form.register("phoneNumber")}
+							autoComplete="tel"
+							fieldError={form.formState.errors.phoneNumber}
+						/>
+					</div>
 					<TextField
-						label="Email"
-						{...form.register("email")}
-						autoComplete="email"
-						fieldError={form.formState.errors.email}
+						label="Address 1"
+						{...form.register("address1")}
+						autoComplete="address-line1"
+						fieldError={form.formState.errors.address1}
 					/>
 					<TextField
-						label="Phone Number"
-						{...form.register("phoneNumber")}
-						autoComplete="tel"
-						fieldError={form.formState.errors.phoneNumber}
+						label="Address 2"
+						{...form.register("address2")}
+						autoComplete="address-line2"
+						fieldError={form.formState.errors.address2}
 					/>
-				</div>
-				<TextField
-					label="Address 1"
-					{...form.register("address1")}
-					autoComplete="address-line1"
-					fieldError={form.formState.errors.address1}
-				/>
-				<TextField
-					label="Address 2"
-					{...form.register("address2")}
-					autoComplete="address-line2"
-					fieldError={form.formState.errors.address2}
-				/>
-				<div className="md:flex">
-					<TextField
-						label="City"
-						{...form.register("city")}
-						autoComplete="address-level2"
-						fieldError={form.formState.errors.city}
+					<div className="md:flex">
+						<TextField
+							label="City"
+							{...form.register("city")}
+							autoComplete="address-level2"
+							fieldError={form.formState.errors.city}
+						/>
+					</div>
+					<div className="md:flex">
+						<TextField
+							label="State"
+							{...form.register("state")}
+							autoComplete="address-level1"
+							fieldError={form.formState.errors.state}
+						/>
+						<TextField
+							label="Zip"
+							{...form.register("zip")}
+							autoComplete="postal-code"
+							fieldError={form.formState.errors.zip}
+						/>
+					</div>
+					<TextArea
+						label="Notes"
+						{...form.register("notes")}
+						fieldError={form.formState.errors.notes}
 					/>
-				</div>
-				<div className="md:flex">
-					<TextField
-						label="State"
-						{...form.register("state")}
-						autoComplete="address-level1"
-						fieldError={form.formState.errors.state}
-					/>
-					<TextField
-						label="Zip"
-						{...form.register("zip")}
-						autoComplete="postal-code"
-						fieldError={form.formState.errors.zip}
-					/>
-				</div>
-				<TextArea
-					label="Notes"
-					{...form.register("notes")}
-					fieldError={form.formState.errors.notes}
-				/>
-			</form>
-		</main>
+				</form>
+			</main>
+		</>
 	);
 };
 
