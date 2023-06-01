fabfcd8c55a1f09d1209569be8beeb0e41efa90b
Claudio Rivera <me@claudiorivera.com>
2023-06-02 05:07:02 +1000

    🔥 Remove loading indicator

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,3 @@
-import { useIsFetching } from "@tanstack/react-query";
-import classNames from "classnames";
 import { ReactNode } from "react";
 
 import { Meta } from "@/components";
@@ -9,17 +7,10 @@ type Props = {
 };
 
 export const Layout = ({ children }: Props) => {
-	const isFetching = useIsFetching();
-
 	return (
 		<>
 			<Meta />
 
-			<div
-				className={classNames("loading", {
-					"animate-none": !isFetching,
-				})}
-			/>
 			<main className="container mx-auto max-w-3xl bg-base-200">
 				{children}
 			</main>
